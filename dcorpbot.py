720fc05c3672df27884e5482532ec6fe0ee94b1f
Daniil <dennico.corp@gmail.com>
2025-06-01 14:47:42 +0300

    eto pizdec

--- dcorpbot.py
+++ dcorpbot.py
@@ -1,1 +1,10 @@
-645654564
+import telebot
+
+bot = telebot.TeleBot('7626248756:AAFaxxxC68vwMaSw9CDQ5E1gfcY6EUZzIH8')
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Иди нахуй')
+
+
+bot.polling(none_stop=True)
