78f7dd251218b1242e12ac4d5c98003969b2a1f1
Daniil <dennico.corp@gmail.com>
2025-06-01 18:25:35 +0300

    eto pizdec

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "7626248756:AAFaxxxC68vwMaSw9CDQ5E1gfcY6EUZzIH8"
 NEURO_API_BASE_URL = "https://openrouter.ai/api/v1"
 NEURO_MODEL_NAME = "deepseek/deepseek-r1-0528-qwen3-8b:free"
-NEURO_API_KEY = "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"
+NEURO_API_KEY = "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"
 DATABASE_URL = "postgresql://user:password@host:port/database"
 DEBUG_MODE = True
